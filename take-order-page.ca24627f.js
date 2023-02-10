c3ec99f37595456a798b40508dd0e9b2c8fa762b
Tonja1 <110766517+Tonja1@users.noreply.github.com>
2023-02-10 20:38:59 +0000

    Deploying to gh-pages from @ Tonja1/first-project@b044c9945ba2d3f9ef7f1dfb58b33477e5a58908 🚀

---
+++ take-order-page.ca24627f.js
@@ -0,0 +1,2 @@
+!function(){var e={openBuyBtn:document.querySelector("[data-buy-open]"),closeBuyBtn:document.querySelector("[data-buy-close]"),buy:document.querySelector("[data-buy]")};function t(){e.buy.classList.toggle("is-hid")}e.openBuyBtn.addEventListener("click",t),e.closeBuyBtn.addEventListener("click",t)}();
+//# sourceMappingURL=take-order-page.ca24627f.js.map
---
+++ take-order-page.fb60adca.js
@@ -0,0 +1,2 @@
+!function(){var d={openAddBtn:document.querySelector("[data-add-open]"),closeAddBtn:document.querySelector("[data-add-close]"),add:document.querySelector("[data-add]")};function e(){d.add.classList.toggle("is-hidd")}d.openAddBtn.addEventListener("click",e),d.closeAddBtn.addEventListener("click",e)}();
+//# sourceMappingURL=take-order-page.fb60adca.js.map
